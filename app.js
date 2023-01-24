455ba50ff0ee6adfbac1843c606a01700dc55235
stefko-tsonyovski <116901971+stefko-tsonyovski@users.noreply.github.com>
2023-01-24 15:44:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 
 // extra security packages
-const helmet = require("helmet");
 const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
@@ -57,7 +56,6 @@ app.use(express.static(path.resolve(__dirname, "./client/build")));
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
-app.use(helmet());
 app.use(cors());
 app.use(xss());
 
